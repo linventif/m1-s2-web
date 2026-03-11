9e9a299338290a0b1b09d284e50a632100dd278f
linventif <gregoire.becue@pm.me>
2026-03-11 16:09:10 +0100

    ex: 2

--- tp/src/main/java/utc/miage/tp/FormController.java
+++ tp/src/main/java/utc/miage/tp/FormController.java
@@ -2,6 +2,9 @@ package utc.miage.tp;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
 @Controller
 public class FormController {
@@ -11,4 +14,10 @@ public class FormController {
 		return "hello";
 	}
 
+	@PostMapping("/hello/form")
+	public String helloSubmit(@RequestParam String name, Model model) {
+		model.addAttribute("name", name);
+		System.out.println("Received name: " + name);
+		return "hello";
+	}
 }
